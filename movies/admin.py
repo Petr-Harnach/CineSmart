25a770768bae5c3f93d8ac25c736e0d299ce20a3
Assasin238238 <harnach2006@gmail.com>
2026-01-13 21:57:37 +0100

    seasons

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Genre, Director, CustomUser, Review, Actor, Screenwriter
+from .models import Movie, Genre, Director, CustomUser, Review, Actor, Screenwriter, Season, Episode
 from django.contrib.auth.admin import UserAdmin
 
 
@@ -13,12 +13,32 @@ class ScreenwriterAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 
+class EpisodeInline(admin.TabularInline):
+    model = Episode
+    extra = 1
+
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'movie', 'season_number', 'release_date')
+    list_filter = ('movie',)
+    search_fields = ('movie__title', 'title')
+    inlines = [EpisodeInline]
+
+
+class SeasonInline(admin.TabularInline):
+    model = Season
+    extra = 0
+    show_change_link = True # Allow clicking through to edit episodes
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'get_genres', 'get_directors')
-    list_filter = ('genres', 'directors')
+    list_display = ('title', 'release_date', 'type', 'get_genres', 'get_directors')
+    list_filter = ('type', 'genres', 'directors')
     search_fields = ('title', 'description', 'directors__name')
-    filter_horizontal = ('genres', 'directors', 'actors', 'screenwriters') # Better UI for M2M
+    filter_horizontal = ('genres', 'directors', 'actors', 'screenwriters')
+    inlines = [SeasonInline] # Add seasons directly to movie
 
     def get_genres(self, obj):
         return ", ".join([g.name for g in obj.genres.all()])
